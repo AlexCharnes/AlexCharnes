f9859bca1181797f9b9ca28f04c29457c4d02395
Brian Lauvray <brian@techelevator.com>
2024-01-19 12:54:39 -0500

    lecture code for loops and arrays

--- module-1/04_Loops_and_Arrays/instructor/src/main/java/com/techelevator/Exercise01_StoreOrders.java
+++ module-1/04_Loops_and_Arrays/instructor/src/main/java/com/techelevator/Exercise01_StoreOrders.java
@@ -43,7 +43,11 @@ public class Exercise01_StoreOrders {
 	 createOrder() → [10, 40, 31, 41]
      */
     public int[] createOrder() {
-        return new int[] {};
+        // 1. Create an array
+        // 2. put in the values
+        int[] order = new int[] { SMALL_CHEESE, CALZONE, LARGE_PEPPERONI, SPAGHETTI_PIE};
+        // 3. return the array
+        return order;
     }
 
     /*
@@ -61,7 +65,19 @@ public class Exercise01_StoreOrders {
     getCalzoneSales([]) → 0
      */
     public int getCalzoneSales(int[] orders) {
-        return 0;
+        // 1. declare a variable to count the number of calzone
+        // 2. loop over the order array
+            // 2a. look for calzone
+            // 2b. if find calzone add 1 to count
+        // 3. return the number of calzones we found
+        int count = 0;
+
+        for (int i = 0; i < orders.length; i++) {
+            if (orders[i] == CALZONE) {
+                count++;
+            }
+        }
+        return count;
     }
 
     /*
@@ -79,6 +95,25 @@ public class Exercise01_StoreOrders {
     getCheesePizzaRevenue([SMALL_PEPPERONI, MEDIUM_PEPPERONI]) → 0
      */
     public int getCheesePizzaRevenue(int[] orders) {
-        return 0;
+        // 1. create a variable to sum the revenue
+        int revenueOfCheesePizzas = 0;
+        // 2. Loop over orders
+            // 2a. initialize the counter variable in for
+            // 2b. check element at the index of i is a Small Cheese Pizza add 8 to revenue
+            // 3b. check element at the index of i is a Medium Cheese Pizza add 11 to revenue
+            // 4b. check element at the index of i is a Large Cheese Pizza add 14 to revenue
+        for (int i = 0; i < orders.length; i++) {
+            if (orders[i] == SMALL_CHEESE) {
+                revenueOfCheesePizzas += 8;
+            } else if (orders[i] == MEDIUM_CHEESE) {
+                revenueOfCheesePizzas += 11;
+            } else if (orders[i] == LARGE_CHEESE) {
+                revenueOfCheesePizzas += 14;
+            }
+        }
+
+
+        // 3. return the revenue
+        return revenueOfCheesePizzas;
     }
 }
