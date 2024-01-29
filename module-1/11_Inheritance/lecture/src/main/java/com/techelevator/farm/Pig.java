78331ddfa9abbe10264717135a0f304936d501b8
Alex Charnes <acharnes7817@gmail.com>
2024-01-29 12:27:06 -0500

    after lecture 1/29/24

---
+++ module-1/11_Inheritance/lecture/src/main/java/com/techelevator/farm/Pig.java
@@ -0,0 +1,12 @@
+package com.techelevator.farm;
+
+public class Pig extends FarmAnimal {
+
+    public Pig() {
+        super("Pig", "Oink");
+    }
+
+    public void rollInMud() {
+        System.out.println("The Pig Rolls in the Mud");
+    }
+}
