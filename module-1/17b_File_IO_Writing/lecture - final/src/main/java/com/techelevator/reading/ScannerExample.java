8e16daaef57662bb16b4e2ba2e966520da66691e
Brian Lauvray <brian@techelevator.com>
2024-02-06 12:02:52 -0500

    lecture code

--- module-1/17b_File_IO_Writing/lecture - final/src/main/java/com/techelevator/reading/ScannerExample.java
+++ module-1/17b_File_IO_Writing/lecture - final/src/main/java/com/techelevator/reading/ScannerExample.java
@@ -1,5 +1,9 @@
 package com.techelevator.reading;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class ScannerExample {
 
     public static void main(String[] args) {
@@ -12,26 +16,30 @@ public class ScannerExample {
        /*
            STEP 2: create a file object and pass it the filename
         */
-
+        File ourFile = new File(filename);
 
        /*
            STEP 3: Open the file with a Scanner in a try-with-resource
         */
-
+        try (Scanner fileScanner = new Scanner(ourFile)) {
            /*
                STEP 4: Loop while hasNextLine() is true
             */
+            while (fileScanner.hasNextLine()) {
 
                 // STEP 5: use nextLine() to read the next line from the file
+                String lineFromTheFile = fileScanner.nextLine();
 
                 // The println() is just here so we can see it and not part of the process
+                System.out.println(lineFromTheFile);
+            }
 
-
-
-
+        } catch (FileNotFoundException e) {
+            System.out.println("The File was not found");
            /*
                STEP 6: handle the FileNotFoundException
             */
+        }
 
     }
 }
--- module-1/17b_File_IO_Writing/lecture - final/src/main/java/com/techelevator/writing/BufferedWriterExample.java
+++ module-1/17b_File_IO_Writing/lecture - final/src/main/java/com/techelevator/writing/BufferedWriterExample.java
@@ -1,5 +1,7 @@
 package com.techelevator.writing;
 
+import java.io.*;
+
 public class BufferedWriterExample {
 
     public static void main(String[] args) {
@@ -7,13 +9,27 @@ public class BufferedWriterExample {
         /*
          * Create a file and PrintWriter
          */
-
+        File ourBufferedFile = new File("bufferedFile.txt");
 
         /*
          * To add buffering a Print Writer is still needed and will still do the writing.
          * A BufferedWriter is added and passed the Print Writer.  The Print Writer will still
          * write to the file and the buffered writer will provide buffering.
          */
+        try ( PrintWriter printWriter = new PrintWriter(ourBufferedFile);
+             BufferedWriter bufferedWriter = new BufferedWriter(printWriter) ) {
+
+            bufferedWriter.write("This is our first line of buffered text");
+            bufferedWriter.newLine();
+
+            bufferedWriter.write("This is the second line");
+           bufferedWriter.newLine();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Your file was not found");
+        } catch (IOException e) {
+            System.out.println("Error writing to file: " + e.getMessage());
+        }
 
     }
 }
