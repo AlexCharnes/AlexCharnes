b34c460450fa4bbd2e81132904d7d79631c55425
Alex Charnes <acharnes7817@gmail.com>
2024-02-07 08:59:01 -0500

    HW turn in

--- module-1/17b_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
+++ module-1/17b_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
@@ -1,5 +1,6 @@
 package com.techelevator;
 
+import java.io.*;
 import java.util.Scanner;
 
 public class FindAndReplace {
@@ -7,13 +8,57 @@ public class FindAndReplace {
     // Use this scanner for all user input. Don't create additional Scanners with System.in
     private final Scanner userInput = new Scanner(System.in);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, FileNotFoundException {
         FindAndReplace findAndReplace = new FindAndReplace();
         findAndReplace.run();
     }
 
-    public void run() {
+    public void run() throws IOException {
         /* Your code goes here */
+
+        System.out.print("What is the search word? ");
+        String wordBeingReplaced = userInput.nextLine();
+
+        System.out.print("What is the replacement word? ");
+        String newWord = userInput.nextLine();
+
+        System.out.print("What is the source file? ");
+        String inputFileName = userInput.nextLine();
+
+        System.out.print("What is the destination file? ");
+        String outputFileName = userInput.nextLine();
+
+        File inputFile = new File(inputFileName);
+
+        File outputFile = new File(outputFileName);
+
+
+
+
+
+        if (!inputFile.exists()){
+            System.out.println("This is an invalid file selection");
+        }
+        try (Scanner textScanner = new Scanner(inputFile);
+             PrintWriter printWriter = new PrintWriter(outputFile);){
+            while (textScanner.hasNextLine()){
+                String line = textScanner.nextLine();
+                if (line.contains(wordBeingReplaced)){
+                    line = line.replaceAll(wordBeingReplaced, newWord);
+                }
+                printWriter.println(line);
+
+
+
+            }
+
+
+
+        }catch (FileNotFoundException e){
+            System.out.println("Input file not found");
+        }catch (IOException e){
+            System.out.println("Error writing to file");
+        }
 
 
     }
--- module-1/17b_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
+++ module-1/17b_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
@@ -1,21 +1,47 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class FizzWriter {
 
-	// Use this scanner for all user input. Don't create additional Scanners with System.in
-	private final Scanner userInput = new Scanner(System.in);
+    // Use this scanner for all user input. Don't create additional Scanners with System.in
+    private final Scanner userInput = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		FizzWriter fizzWriter = new FizzWriter();
-		fizzWriter.run();
-	}
+    public static void main(String[] args) throws FileNotFoundException {
+        FizzWriter fizzWriter = new FizzWriter();
+        fizzWriter.run();
+    }
 
-	public void run() {
-		/* Your code goes here */
+    public void run() throws FileNotFoundException {
+
+        /* Your code goes here */
+
+        System.out.print("Output file: ");
+        String outputFileName = userInput.nextLine();
+
+        File outputFile = new File(outputFileName);
+
+        try (PrintWriter fizzWriter = new PrintWriter(outputFile)) {
+
+            for (int i = 1; i <= 300; i++) {
+                if (i % 3 == 0 && i % 5 == 0) {
+                    fizzWriter.println("FizzBuzz");
+                } else if (i % 5 == 0) {
+                    fizzWriter.println("Buzz");
+                } else if (i % 3 == 0) {
+                    fizzWriter.println("Fizz");
+                } else {
+                    fizzWriter.println(i);
+                }
+            }
 
 
-	}
+        } catch (FileNotFoundException e) {
+            System.out.println("Output file not found: " + outputFileName);
+        }
 
+    }
 }
