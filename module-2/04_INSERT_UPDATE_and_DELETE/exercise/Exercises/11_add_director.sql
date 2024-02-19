5757beed4612c98af7f43dc8388defd7c87b92cc
Alex Charnes <acharnes7817@gmail.com>
2024-02-19 13:18:41 -0500

    pull

--- module-2/04_INSERT_UPDATE_and_DELETE/exercise/Exercises/11_add_director.sql
+++ module-2/04_INSERT_UPDATE_and_DELETE/exercise/Exercises/11_add_director.sql
@@ -1,3 +1,14 @@
--- 11. Hollywood is remaking the classic movie "The Blob" and doesn't have a director yet. Add yourself to the person
---     table, and assign yourself as the director of "The Blob" (the movie is already in the movie table). (1 record each)
+-- 11. Hollywood is remaking the classic movie "The Blob" and doesn't have a director yet. 
+-- Add yourself to the person table, and assign yourself as the director of "The Blob" 
+-- (the movie is already in the movie table). (1 record each)
 
+START TRANSACTION;
+
+INSERT INTO person(person_name)
+VALUES ('Alex Charnes');
+
+UPDATE movie 
+SET director_id = 3984923
+WHERE title = 'The Blob';
+
+COMMIT;
