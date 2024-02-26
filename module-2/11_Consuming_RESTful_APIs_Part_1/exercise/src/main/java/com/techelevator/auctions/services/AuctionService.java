6df6266d236b02ce032b9e79a7b815e97d0c086b
Alex Charnes <acharnes7817@gmail.com>
2024-02-26 16:17:17 -0500

    push

--- module-2/11_Consuming_RESTful_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/services/AuctionService.java
+++ module-2/11_Consuming_RESTful_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/services/AuctionService.java
@@ -1,5 +1,6 @@
 package com.techelevator.auctions.services;
 
+import jdk.jfr.consumer.RecordedStackTrace;
 import org.springframework.web.client.RestTemplate;
 
 import com.techelevator.auctions.model.Auction;
@@ -14,22 +15,28 @@ public class AuctionService {
 
         // call api here
         Auction[] response = restTemplate.getForObject(API_BASE_URL, Auction[].class);
-        return null;
+        return response;
     }
 
     public Auction getAuction(int id) {
+        String url = API_BASE_URL + "/" + id;
+
         // call api here
-        return null;
+        return restTemplate.getForObject(url, Auction.class);
     }
 
     public Auction[] getAuctionsMatchingTitle(String title) {
+
+        String url = API_BASE_URL + "?title_like=" + title;
         // call api here
-        return null;
+        return restTemplate.getForObject(url, Auction[].class);
     }
 
     public Auction[] getAuctionsAtOrBelowPrice(double price) {
+        //http://localhost:3000/auctions?currentBid_lte=<*value*>
+        String url = API_BASE_URL + "?currentBid_lte=" + price;
         // call api here
-        return null;
+        return restTemplate.getForObject(url,Auction[].class);
     }
 
 }
