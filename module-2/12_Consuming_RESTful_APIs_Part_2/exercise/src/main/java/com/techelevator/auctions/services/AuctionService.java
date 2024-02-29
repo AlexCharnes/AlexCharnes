20c8e3d5818624fc134eb751c7d965146338ebec
Alex Charnes <acharnes7817@gmail.com>
2024-02-29 09:09:42 -0500

    push

--- module-2/12_Consuming_RESTful_APIs_Part_2/exercise/src/main/java/com/techelevator/auctions/services/AuctionService.java
+++ module-2/12_Consuming_RESTful_APIs_Part_2/exercise/src/main/java/com/techelevator/auctions/services/AuctionService.java
@@ -17,13 +17,49 @@ public class AuctionService {
 
 
     public Auction add(Auction newAuction) {
+
+        // 2) Instantiate an HttpHeaders
+        HttpHeaders httpHeaders = new HttpHeaders();
+        // 3) Set the ContentType to application/json
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        // 4) Instantiate an HttpEntity<T> T - the data type to serialize into the message body JSON
+        //        new HttpEntity<T>(object to serialize, headers)
+        HttpEntity<Auction> httpRequest = new HttpEntity<Auction>(newAuction, httpHeaders);
+
+        Auction returnedAuction = null;
+        try {
+            returnedAuction = restTemplate.postForObject(API_BASE_URL, httpRequest, Auction.class);
+        } catch (ResourceAccessException e) {
+            // Cannot connect to the server
+            BasicLogger.log("Unable to connect to server: " + e.getMessage());
+        } catch (RestClientResponseException e) {
+            // Response from server, but was an error code
+            BasicLogger.log(e.getRawStatusCode() + " : " + e.getStatusText());
+        }
+
+        return returnedAuction;
         // place code here
-        return null;
+
     }
 
     public boolean update(Auction updatedAuction) {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<Auction> requestEntity = new HttpEntity<Auction>(updatedAuction, httpHeaders);
+
+        boolean isSuccessful = false;
+        try {
+            restTemplate.put(API_BASE_URL + "/" + updatedAuction.getId(), requestEntity);
+            isSuccessful = true;
+        } catch (ResourceAccessException e) {
+            BasicLogger.log("Can not connect to server");
+        } catch (RestClientResponseException e) {
+            BasicLogger.log(e.getRawStatusCode() + " : " + e.getStatusText());
+        }
+
+        return isSuccessful;
         // place code here
-        return false;
+
     }
 
     public boolean delete(int auctionId) {
